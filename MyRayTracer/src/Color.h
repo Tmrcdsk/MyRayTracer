4b2a44be36faa1705247b6c10eace69b26df3504
Tmrcdsk <129770406+Tmrcdsk@users.noreply.github.com>
2025-03-08 12:48:40 +0800

    Gamma校正

--- MyRayTracer/src/Color.h
+++ MyRayTracer/src/Color.h
@@ -5,10 +5,20 @@
 
 using color = vec3;
 
+inline float linearToGamma(float linearComponent) {
+	if (linearComponent > 0.0f)
+		return std::sqrtf(linearComponent); // gamma = 2 (2 for simplicity, usually set to 2.2)
+	return 0.0f;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
 	auto r = pixel_color.x;
 	auto g = pixel_color.y;
 	auto b = pixel_color.z;
+
+	r = linearToGamma(r);
+	g = linearToGamma(g);
+	b = linearToGamma(b);
 
 	// [0.0, 1.0] -> [0, 255]
 	static const Interval intensity(0.000f, 0.999f);
