138bfe08b08c8c100ae9c089a42cd16f5194e596
Tmrcdsk <129770406+Tmrcdsk@users.noreply.github.com>
2025-09-08 20:34:49 +0800

    3.6 创建 HittableList 的边界框

--- MyRayTracer/src/HittableList.h
+++ MyRayTracer/src/HittableList.h
@@ -2,18 +2,24 @@
 
 #include "Global.h"
 #include "Hittable.h"
+#include "AABB.h"
 
 #include <vector>
 
 class HittableList : public Hittable
 {
 public:
+	std::vector<std::shared_ptr<Hittable>> objects;
+
 	HittableList() = default;
 	HittableList(std::shared_ptr<Hittable> object) { add(object); }
 
 	void clear() { objects.clear(); }
 
-	void add(std::shared_ptr<Hittable> object) { objects.push_back(object); }
+	void add(std::shared_ptr<Hittable> object) {
+		objects.push_back(object);
+		bbox = AABB(bbox, object->boundingBox());
+	}
 
 	bool hit(const Ray& ray, Interval t, HitPayload& payload) const {
 		HitPayload tmpPayload;
@@ -31,5 +37,8 @@ public:
 		return hitAnything;
 	}
 
-	std::vector<std::shared_ptr<Hittable>> objects;
+	AABB boundingBox() const override { return bbox; }
+
+private:
+	AABB bbox;
 };
