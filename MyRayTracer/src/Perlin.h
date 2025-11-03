1e5a6a815367e7fd80e86815de9e8bd97c1972e4
Tmrcdsk <1345254469@qq.com>
2025-11-03 11:17:12 +0800

    5.5 在格点上使用随机向量

--- MyRayTracer/src/Perlin.h
+++ MyRayTracer/src/Perlin.h
@@ -7,7 +7,7 @@ class Perlin
 public:
 	Perlin() {
 		for (int i = 0; i < point_count; ++i) {
-			randfloat[i] = randomFloat();
+			randvec[i] = randomUnit();
 		}
 
 		perlinGeneratePerm(perm_x);
@@ -19,31 +19,28 @@ public:
 		auto u = p.x - std::floor(p.x);
 		auto v = p.y - std::floor(p.y);
 		auto w = p.z - std::floor(p.z);
-		u = u * u * (3 - 2 * u);
-		v = v * v * (3 - 2 * v);
-		w = w * w * (3 - 2 * w);
 
 		auto i = int(std::floor(p.x));
 		auto j = int(std::floor(p.y));
 		auto k = int(std::floor(p.z));
-		float c[2][2][2];
+		vec3 c[2][2][2];
 
 		for (int di = 0; di < 2; ++di)
 			for (int dj = 0; dj < 2; ++dj)
 				for (int dk = 0; dk < 2; ++dk) {
-					c[di][dj][dk] = randfloat[
+					c[di][dj][dk] = randvec[
 						perm_x[(i + di) & 255] ^
 						perm_y[(j + dj) & 255] ^
 						perm_z[(k + dk) & 255]
 					];
 				}
 		
-		return trilinearInterp(c, u, v, w);
+		return perlinInterp(c, u, v, w);
 	}
 
 private:
 	static const int point_count = 256;
-	float randfloat[point_count];
+	vec3 randvec[point_count];
 	int perm_x[point_count];
 	int perm_y[point_count];
 	int perm_z[point_count];
@@ -64,15 +61,20 @@ private:
 		}
 	}
 
-	static float trilinearInterp(float c[2][2][2], float u, float v, float w) {
+	static float perlinInterp(const vec3 c[2][2][2], float u, float v, float w) {
+		auto uu = u * u * (3 - 2 * u);
+		auto vv = v * v * (3 - 2 * v);
+		auto ww = w * w * (3 - 2 * w);
+
 		float accum = 0.0f;
 		for (int i = 0; i < 2; ++i)
 			for (int j = 0; j < 2; ++j)
 				for (int k = 0; k < 2; ++k) {
-					accum += (i * u + (1 - i) * (1 - u))
-						   * (j * v + (1 - j) * (1 - v))
-						   * (k * w + (1 - k) * (1 - w))
-						   * c[i][j][k];
+					vec3 weight_v(u - i, v - j, w - k);
+					accum += (i * uu + (1 - i) * (1 - uu))
+						   * (j * vv + (1 - j) * (1 - vv))
+						   * (k * ww + (1 - k) * (1 - ww))
+						   * dot(c[i][j][k], weight_v);
 				}
 		return accum;
 	}
