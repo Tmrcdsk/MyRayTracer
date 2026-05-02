99f39e6ae8bec0772e9c55701f2bcf461357b00c
Tmrcdsk <129770406+Tmrcdsk@users.noreply.github.com>
2026-05-02 14:32:32 +0800

    update README.md

--- MyRayTracer/src/Camera.h
+++ MyRayTracer/src/Camera.h
@@ -33,7 +33,7 @@ public:
 #if MULTI_THREAD == 1
 		std::vector<vec3> framebuffer(Width * Height);
 
-		const int numThreads = std::thread::hardware_concurrency() / 2;
+		const int numThreads = std::thread::hardware_concurrency();
 		std::vector<std::thread> threads;
 		threads.reserve(numThreads);
 
